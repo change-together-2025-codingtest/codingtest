5696a7f7d49e431c64c1fa57d016b381329782af
taejeong77 <oleh13579@naver.com>
2025-10-01 22:38:15 +0900

    [week1]LeeTaeJeong

---
+++ week1/problem_03.py
@@ -0,0 +1,12 @@
+def solution(arr):
+    answer = []
+    length = len(arr) # 배열 길이를 찾기 위한 변수
+    for i in range(length): # 배열 길이만큼 반복
+        if i==length-1: # 마지막 인덱스일 경우
+            answer.append(arr[i]) # 마지막 인덱스는 무조건 추가
+        elif arr[i]==arr[i+1]: # 현재 인덱스와 다음 인덱스가 같을 경우
+            continue # 다음 인덱스로 넘어감
+        else: 
+            answer.append(arr[i]) # 현재 인덱스와 다음 인덱스가 다를 경우 현재 인덱스를 추가
+
+    return answer
